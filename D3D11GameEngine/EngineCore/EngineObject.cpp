9f7ffa7d85203ba731a70f3abc0416322642f1c5
이한성 <mast1207@naver.com>
2024-07-10 23:20:36 +0900

    업데이트

--- D3D11GameEngine/EngineCore/EngineObject.cpp
+++ D3D11GameEngine/EngineCore/EngineObject.cpp
@@ -17,6 +17,103 @@ EngineObject::~EngineObject()
 	}
 }
 
+void EngineObject::SetName(EngineString _Name)
+{
+	Name = _Name;
+}
+
+void EngineObject::SetParent(EngineObject* _Parent)
+{
+	_Parent->SetChild(this);
+}
+
+void EngineObject::DetachParent()
+{
+	Parent->DetachChild(this);
+}
+
+void EngineObject::SetChild(EngineObject* _Child)
+{
+	// 이미 내 자식이라면 리턴
+	for (EngineObject* Ptr : ChildList)
+	{
+		if (Ptr == _Child)
+		{
+			return;
+		}
+	}
+
+	// 자식의 부모가 있다면 먼저 떼어놓기
+	if (_Child->Parent != nullptr)
+	{
+		_Child->Parent->DetachChild(_Child);
+	}
+	ChildList.push_back(_Child);
+	_Child->Parent = this;
+}
+
+void EngineObject::DetachChild(EngineObject* _Child)
+{
+	std::list<EngineObject*>::iterator Start = ChildList.begin();
+	std::list<EngineObject*>::iterator End = ChildList.end();
+
+	for (; Start != End;)
+	{
+		if ((*Start) == _Child)
+		{
+			ChildList.erase(Start);
+			_Child->Parent = nullptr;
+			return;
+		}
+
+		Start++;
+	}
+}
+
+EngineObject* EngineObject::GetChild(int Order = 0)
+{
+	std::list<EngineObject*>::iterator Start = ChildList.begin();
+	std::list<EngineObject*>::iterator End = ChildList.end();
+
+	EngineObject* Result = nullptr;
+	int index = 0;
+	for (; Start != End;)
+	{
+		if (index == Order)
+		{
+			Result = (*Start);
+			return Result;
+		}
+
+		Start++;
+		index++;
+	}
+
+	return Result;
+}
+
+EngineObject* EngineObject::GetTopParent()
+{
+	if (Parent != nullptr)
+	{
+		return Parent->GetTopParent();
+	}
+	else
+	{
+		return this;
+	}
+}
+
+bool EngineObject::Destroy()
+{
+	Death = true;
+}
+
+bool EngineObject::IsDeath()
+{
+	return Death;
+}
+
 void EngineObject::Start()
 {
 }
--- D3D11GameEngine/EngineCore/EngineObject.h
+++ D3D11GameEngine/EngineCore/EngineObject.h
@@ -14,103 +14,15 @@ public:
 	EngineObject& operator=(const EngineObject& _Other) = delete;
 	EngineObject& operator=(EngineObject&& _Other) noexcept = delete;
 
-	void SetName(EngineString _Name)
-	{
-		Name = _Name;
-	}
-
-	void SetParent(EngineObject* _Parent)
-	{
-		_Parent->SetChild(this);
-	}
-
-	void DetachParent()
-	{
-		Parent->DetachChild(this);
-	}
-
-	void SetChild(EngineObject* _Child)
-	{
-		// 이미 내 자식이라면 리턴
-		for (EngineObject* Ptr : ChildList)
-		{
-			if (Ptr == _Child)
-			{
-				return;
-			}
-		}
-
-		// 자식의 부모가 있다면 먼저 떼어놓기
-		if (_Child->Parent != nullptr)
-		{
-			_Child->Parent->DetachChild(_Child);
-		}
-		ChildList.push_back(_Child);
-		_Child->Parent = this;
-	}
-
-	void DetachChild(EngineObject* _Child)
-	{
-		std::list<EngineObject*>::iterator Start = ChildList.begin();
-		std::list<EngineObject*>::iterator End = ChildList.end();
-
-		for (; Start != End;)
-		{
-			if ((*Start) == _Child)
-			{
-				ChildList.erase(Start);
-				_Child->Parent = nullptr;
-				return;
-			}
-
-			Start++;
-		}
-	}
-
-	EngineObject* GetChild(int Order = 0)
-	{
-		std::list<EngineObject*>::iterator Start = ChildList.begin();
-		std::list<EngineObject*>::iterator End = ChildList.end();
-
-		EngineObject* Result = nullptr;
-		int index = 0;
-		for (; Start != End;)
-		{
-			if (index == Order)
-			{
-				Result = (*Start);
-				return Result;
-			}
-
-			Start++;
-			index++;
-		}
-
-		return Result;
-	}
-
-	EngineObject* GetTopParent()
-	{
-		if (Parent != nullptr)
-		{
-			return Parent->GetTopParent();
-		}
-		else
-		{
-			return this;
-		}
-	}
-
-	bool Destroy()
-	{
-		Death = true;
-	}
-
-	bool IsDeath()
-	{
-		return Death;
-	}
-
+	void SetName(EngineString _Name);
+	void SetParent(EngineObject* _Parent);
+	void DetachParent();
+	void SetChild(EngineObject* _Child);
+	void DetachChild(EngineObject* _Child);
+	EngineObject* GetChild(int Order = 0);
+	EngineObject* GetTopParent();
+	bool Destroy();
+	bool IsDeath();
 	virtual void Start();
 	void ChildUpdate(float _Delta);
 	virtual void Update(float _Delta);
