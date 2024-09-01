58f5133f72baa8a6c38481140cd02f806ac87b83
PC <PC@PC-060>
2024-09-01 14:44:59 +0900

    GetComponent() 추가

--- D3D11GameEngine/CoreLib/EngineActor.cpp
+++ D3D11GameEngine/CoreLib/EngineActor.cpp
@@ -1,2 +1,18 @@
 #include "Pre.h"
 #include "EngineActor.h"
+
+EngineComponent* EngineActor::GetComponent(const char* _Name)
+{
+	UINT Count = ChildList.GetCount();
+	ChildList.GoFirst();
+
+	for (UINT i = 0; i < Count; i++)
+	{
+		EngineComponent* Child = (EngineComponent*)ChildList.Item();
+		if (Child->Name == _Name)
+		{
+			return Child;
+		}
+	}
+	return nullptr;
+}
