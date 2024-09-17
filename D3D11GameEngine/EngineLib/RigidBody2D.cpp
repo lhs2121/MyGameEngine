3edd3e7004977a3c9638c9951097c21ebcbbf9fa
lhs2121 <mast1207@naver.com>
2024-09-17 19:52:12 +0900

    마찰력

--- D3D11GameEngine/EngineLib/RigidBody2D.cpp
+++ D3D11GameEngine/EngineLib/RigidBody2D.cpp
@@ -1,5 +1,6 @@
 #include "Pre.h"
 #include "RigidBody2D.h"
+#include "math.h"
 
 void RigidBody2D::Start()
 {
@@ -13,16 +14,24 @@ void RigidBody2D::Update(float _deltaTime)
 
 void RigidBody2D::LateUpdate(float _deltaTime)
 {
-	float4 f_normal;
+	//일단 지면콜리전의 각도를구하자 그걸 세타라하고
+	//mgsin세타를 수직항력으로 한다면 빗면이든 빗면이 아니든 알짜힘으로 활용함
+	//아 맞다 마찰력도 더해야함
+	float4 f_total;
+
 	if (parentCol->IsCollision())
 	{
-		f_normal = -f_gravity * 5;
-	}
-	
 
-	float4 f_total;
-	f_total += f_gravity;
-	f_total += f_normal;
+		float normalRadian = theta.z* DegToRad;
+		float4 f_slide = c_gravity * sinf(normalRadian);
+		float4 f_normal = -c_gravity * cos(normalRadian);
+
+		float4 f_friction = c_gravity * cos(normalRadian) * c_friction;
+		f_total = f_slide + f_friction + f_normal;
+	}
+	else
+		f_total += c_gravity;
+	
 	acceleration = f_total;
 	velocity += acceleration * _deltaTime;
 	float4 displacement = velocity * _deltaTime;
--- D3D11GameEngine/EngineLib/RigidBody2D.h
+++ D3D11GameEngine/EngineLib/RigidBody2D.h
@@ -11,10 +11,13 @@ public:
 	void Start() override;
 	void Update(float _deltaTime) override;
 	void LateUpdate(float _deltaTime) override;
-	float4 f_gravity = { 0.0f,-9.8f,0.0f,1.0f };
 	float4 acceleration = { 0.0f,0.0f,0.0f,1.0f };
 	float4 velocity = { 0.0f,0.0f,0.0f,1.0f };
-	float m = 50;
+	float4 theta = { 0.0f,0.0f,1.0f,1.0f };
+
+	float4 c_gravity = { 0.0f,-9.8f,0.0f,1.0f }; //중력
+	float c_friction = 0.55f; // 마찰계수
+	float c_m = 1; //질량
 private:
 	Object* parentObject = nullptr;
 	Colider2D* parentCol = nullptr;
